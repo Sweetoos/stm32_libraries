27b7acfbc38d440291e5b7bba36dac3e167564f4
Kacper <thespbsik@gmail.com>
2024-03-18 22:45:08 +0100

    written more in source file

---
+++ stts751lib/Core/Inc/stts751.h
@@ -0,0 +1,18 @@
+#ifndef STTS751_H
+#define STTS751_H
+
+#include "stm32f4xx_hal.h"
+#include <stdint.h>
+
+#define STTS751_I2C_ADDRESS 0xFD
+
+typedef struct
+{
+    I2C_HandleTypeDef *hi2c;
+    uint8_t address;
+} STTS751_HandleTypeDef;
+
+void STTS751_Init(STTS751_HandleTypeDef *hstts751, I2C_HandleTypeDef *hi2c);
+//float STTS751_ReadTemperature(STTS751_HandleTypeDef *hstts751);
+
+#endif
